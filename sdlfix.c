f46ee3abe15f35e20e402fe170b642a25d060af4
Jack <maier.sebastian@gmail.com>
2018-01-25 19:59:38 +0100

    modified pitch calculation

--- sdlfix.c
+++ sdlfix.c
@@ -6,108 +6,89 @@
 #include <stdint.h>
 #include <stdio.h>
 
+int force = 1;
 
-
-#define SDL_FULLSCREEN 0x80000000u
-#define SDL_NOFRAME 0x00000020u
-
-#define Uint8 uint8_t
-#define Uint32 uint32_t
-#define Uint16 uint16_t
-#define Sint16 short int
-
-int force = 0;
-
-
-typedef struct{
-  Uint8 r;
-  Uint8 g;
-  Uint8 b;
-  Uint8 unused;
-} SDL_Color;
+typedef struct SDLPixelFormat {
+  void *palette;
+  uint8_t  BitsPerPixel;
+  uint8_t  BytesPerPixel;
+  uint8_t  Rloss, Gloss, Bloss, Aloss;
+  uint8_t  Rshift, Gshift, Bshift, Ashift;
+  uint32_t Rmask, Gmask, Bmask, Amask;
+  uint32_t colorkey;
+  uint8_t  alpha;
+} SDLPixelFormat;
 
 typedef struct{
-  Sint16 x, y;
-  Uint16 w, h;
-} SDL_Rect;
-
-typedef struct{
-  int ncolors;
-  SDL_Color *colors;
-} SDL_Palette;
-
-typedef struct SDL_PixelFormat {
-  SDL_Palette *palette;
-  Uint8  BitsPerPixel;
-  Uint8  BytesPerPixel;
-  Uint8  Rloss, Gloss, Bloss, Aloss;
-  Uint8  Rshift, Gshift, Bshift, Ashift;
-  Uint32 Rmask, Gmask, Bmask, Amask;
-  Uint32 colorkey;
-  Uint8  alpha;
-} SDL_PixelFormat;
-
-
-
-typedef struct{
-    Uint32 hw_available:1;
-    Uint32 wm_available:1;
-    Uint32 blit_hw:1;
-    Uint32 blit_hw_CC:1;
-    Uint32 blit_hw_A:1;
-    Uint32 blit_sw:1;
-    Uint32 blit_sw_CC:1;
-    Uint32 blit_sw_A:1;
-    Uint32 blit_fill:1;
-    Uint32 video_mem;
-    SDL_PixelFormat *vfmt;
+    uint32_t hw_available:1;
+    uint32_t wm_available:1;
+    uint32_t blit_hw:1;
+    uint32_t blit_hw_CC:1;
+    uint32_t blit_hw_A:1;
+    uint32_t blit_sw:1;
+    uint32_t blit_sw_CC:1;
+    uint32_t blit_sw_A:1;
+    uint32_t blit_fill:1;
+    uint32_t video_mem;
+    void *vfmt;
     int current_w;
     int current_h;
-} SDL_VideoInfo;
+} SDLVideoInfo;
 
 
 typedef struct SDL_Surface
 {
-    Uint32 flags;              
-    SDL_PixelFormat *format;   
+    uint32_t flags;              
+    SDLPixelFormat *format;   
     int w, h;                   
     int pitch;                  
     void *pixels;    
     void *userdata;   
     int locked;                 
-    void *lock_data;       
-    SDL_Rect clip_rect;      
-    struct SDL_BlitMap *map;        
-    int refcount;               
-} SDL_Surface;
+    void *lock_data;      
+         
+} SDLSurface;
 
 
-SDL_Surface* screen = NULL;
-SDL_Surface* realScreen = NULL;
+SDLSurface* screen = NULL;
+SDLSurface* realScreen = NULL;
 
 void updateScreen(void)
-{      
+{    
+
+  //printf("size of pixel array %lu %d\n", (unsigned long)sizeof(realScreen->pixels), screen->pitch);  
   //printf("update screen %dx %d %d %ld\n", screen->w, screen->h, screen->pitch, sizeof(screen->pixels)/sizeof(screen->pixels[0]));
   //printf("update real screen %dx %d %d %ld\n", realScreen->w, realScreen->h, realScreen->pitch, sizeof(realScreen->pixels) );
+  
+  int pitch = screen->w * screen->format->BytesPerPixel;
+  int dataWith = sizeof(screen->pixels[0]);
+
+   
+  
+
    int i;
     for(i=0; i < 240; i++) {
-        int offset = 2 * i;
-        memcpy(((Uint8 *)realScreen->pixels) + (offset * screen->pitch ), ((Uint8 *)screen->pixels) + i * screen->pitch, screen->pitch);
-        memcpy(((Uint8 *)realScreen->pixels) + (offset + 1) * screen->pitch , ((Uint8 *)screen->pixels) + i *  screen->pitch, screen->pitch);           
+        int offset = 2 * i;      
+        memcpy(((uint8_t *)realScreen->pixels) + (offset * pitch * dataWith ), ((uint8_t *)screen->pixels) + i * pitch * dataWith, pitch);
+        memcpy(((uint8_t *)realScreen->pixels) + (offset + 1) * pitch * dataWith , ((uint8_t *)screen->pixels) + i *  pitch * dataWith, pitch);   
+     
+       //  memcpy(((int *)realScreen->pixels) + (offset + 1) * screen->pitch , ((int *)screen->pixels) + i *  screen->pitch, screen->pitch);   
     }  
-    //printf("update screen finish\n");
+   // printf("update screen finish\n");
+   
 }
 
 
 void* SDL_GetVideoInfo() { 
   static void*(*info)() = NULL;
+   printf("Call to SDL_GetVideoInfo intercepted\n");
 
   if (info == NULL) {   
     info = dlsym(RTLD_NEXT, "SDL_GetVideoInfo");
     assert(info != NULL);
   }
 
-  SDL_VideoInfo* inf = (SDL_VideoInfo*)info();   //<-- calls SDL_GetVideoInfo();   
+  SDLVideoInfo* inf = (SDLVideoInfo*)info();   //<-- calls SDL_GetVideoInfo();   
   int screenWidth = inf->current_w;
   int screenHeight = inf->current_h;
 
@@ -115,7 +96,9 @@ void* SDL_GetVideoInfo() {
     printf("Call to SDL_GetVideoInfo %d x %d\n", screenWidth, screenHeight );
     printf("correcting to 320x240\n");
     inf->current_h = 240;
-  } 
+  } else {
+     printf("Call to SDL_GetVideoInfo intercepted\n");
+  }
   
   return inf;
 }
@@ -125,12 +108,13 @@ void* SDL_GetVideoInfo() {
 void* SDL_SetVideoMode(int width, int height, int bitsperpixel, uint32_t flags) {
   static void*(*real_func)(int,int,int,uint32_t) = NULL;
   static void*(*info)() = NULL;
-  static void*(*create)(Uint32 , int, int, int,Uint32 , Uint32 ,  Uint32 , Uint32 ) =NULL;
+  static void*(*create)(uint32_t , int, int, int,uint32_t , uint32_t ,  uint32_t , uint32_t ) =NULL;
+
+   printf("Call to SDL_SetVideoMode intercepted\n");
+    
 
   if (real_func == NULL) {
-    real_func = dlsym(RTLD_NEXT, "SDL_SetVideoMode");
-    info = dlsym(RTLD_NEXT, "SDL_GetVideoInfo");
-    create = dlsym(RTLD_NEXT, "SDL_CreateRGBSurface");
+    real_func = dlsym(RTLD_NEXT, "SDL_SetVideoMode");    
     assert(real_func != NULL);
   }
 
@@ -144,31 +128,36 @@ void* SDL_SetVideoMode(int width, int height, int bitsperpixel, uint32_t flags) 
      assert(create != NULL);
   }
 
-
-  const SDL_VideoInfo* inf = (SDL_VideoInfo*)info();   //<-- calls SDL_GetVideoInfo();   
+  const SDLVideoInfo* inf = (SDLVideoInfo*)info();   //<-- calls SDL_GetVideoInfo();   
   int screenWidth = inf->current_w;
   int screenHeight = inf->current_h;
 
+
+
   if( (screenWidth == 320 && screenHeight == 480 && width == 320 && height == 240) || (width == 320 && height ==480) || force )  {
     printf("Call to SDL_SetVideoMode %d x %d, corrected screen to 320x240\n", width, height );
-    screen = (SDL_Surface*)create(0, 320, 240, bitsperpixel, 0, 0, 0, 0);
-    realScreen = (SDL_Surface*)real_func(320, 480, bitsperpixel, flags);
+    screen = (SDLSurface*)create(0, 320, 240, bitsperpixel, 0, 0, 0, 0);
+    realScreen = (SDLSurface*)real_func(320, 480, bitsperpixel, flags);
     return screen;
   } else {
-     realScreen = (SDL_Surface*)real_func(width, height, bitsperpixel, flags);
+     printf("Call to SDL_SetVideoMode intercepted, real screen was used\n");
+     realScreen = (SDLSurface*)real_func(width, height, bitsperpixel, flags);
   }
 
   
   return realScreen;
 }
 
+static void*(*flip)() = NULL;
+
 void* SDL_Flip(void* surface) { 
-  static void*(*flip)() = NULL;
- 
+ //  printf("Call to SDL_Flip intercepted\n");
+
   if (flip == NULL) {   
     flip = dlsym(RTLD_NEXT, "SDL_Flip");
     assert(flip != NULL);
-  }    
+  }   
+
 
   if(surface == realScreen) {
         //printf("flipping real screen\n");
@@ -182,5 +171,3 @@ void* SDL_Flip(void* surface) {
   } 
  
 }
-
-
